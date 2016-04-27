26146ac74afc9caa597325aeeab041d919fa89f5
yubaoguang <475209990@qq.com>
2016-04-27 23:26:46 +0800

    悬浮按钮的样式设计以及点击事件

--- app/src/main/java/edu/design/ygb/memo/MainActivity.java
+++ app/src/main/java/edu/design/ygb/memo/MainActivity.java
@@ -1,5 +1,7 @@
 package edu.design.ygb.memo;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,25 +14,74 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionMenu;
+import com.oguzdev.circularfloatingactionmenu.library.SubActionButton;
+
+import edu.design.ygb.memo.TextMemo.TextMemoActivity;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        context=getApplicationContext();
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+
+        SubActionButton.Builder itemBuilder = new SubActionButton.Builder(this);// 文本
+        ImageView itemIcon = new ImageView(this);
+        itemIcon.setImageDrawable(getResources().getDrawable(R.drawable.ic_menu_camera));
+        SubActionButton button1 = itemBuilder.setContentView(itemIcon).build();
+        button1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(context, TextMemoActivity.class));
+                    }
+                }
+
+        );
+
+
+
+
+        SubActionButton.Builder itemBuilder2 = new SubActionButton.Builder(this);// 图片
+        ImageView itemIcon2 = new ImageView(this);
+        itemIcon2.setImageDrawable(getResources().getDrawable(R.drawable.ic_menu_gallery));
+        SubActionButton button2 = itemBuilder2.setContentView(itemIcon2).build();
+
+        SubActionButton.Builder itemBuilder3 = new SubActionButton.Builder(this);// 语音
+        ImageView itemIcon3 = new ImageView(this);
+        itemIcon3.setImageDrawable(getResources().getDrawable(R.drawable.ic_menu_manage));
+        SubActionButton button3 = itemBuilder.setContentView(itemIcon3).build();
+
+        SubActionButton.Builder itemBuilder4 = new SubActionButton.Builder(this);// 视频
+        ImageView itemIcon4 = new ImageView(this);
+        itemIcon4.setImageDrawable(getResources().getDrawable(R.drawable.ic_menu_send));
+        SubActionButton button4 = itemBuilder4.setContentView(itemIcon4).build();
+
+
+        FloatingActionMenu actionMenu = new FloatingActionMenu.Builder(this)
+                .addSubActionView(button1)
+                .addSubActionView(button2)
+                .addSubActionView(button3)
+                .addSubActionView(button4)
+                .attachTo(fab)
+                .build();
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
